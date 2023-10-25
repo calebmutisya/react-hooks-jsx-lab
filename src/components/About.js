fb2eff1f28d914af62f04af93372437981e9fd67
calebmutisya <mutisyacalebmusau@gmail.com>
2023-10-25 12:31:49 +0300

    Completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="about">
-    <h2>About Me</h2>
-    <p>Best dev</p>
-    <img src={image} alt="I made this"/>
-  </div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Best dev</p>
+      <img src={image} alt="I made this"/>
+    </div>
+  ) 
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div id="home">
-    <h1>{name} is a Web Developer from Your {city}</h1>
-  </div>;
+  return (
+    <div id="home">
+      <h1 style={{color:"firebrick"}}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  ) 
 }
 
 export default Home;
